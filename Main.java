2db8023f6d17b11005950c62bd5e469d17f18d87
jrincon00 <jonanderincon@gmail.com>
2025-05-10 14:03:04 +0200

    Cambios en los main y css

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hola, mundo!");
+    System.out.println("Hola, mundo")
+
+  sd
+
+    sad
+    sd
+    sd
+
+    sd
+    sd
+    sd
+    sd
+    sd
+    
   }
 }
