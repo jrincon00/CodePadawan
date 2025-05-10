c943d62d318fecdae770aacebce2fe3604c91966
jrincon00 <jonanderincon@gmail.com>
2025-05-10 17:44:00 +0200

    Main corregido

--- Main.java
+++ Main.java
@@ -1,19 +1,6 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hola, mundo")
-
-  sd
-
-    sad
-    sd
-    sd
-
-    sd
-    sd
-    sd
-    sd
-    sd
-    
+    System.out.println("Hola, mundo");
   }
 }
